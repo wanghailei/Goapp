42606b1a2b5f73aeccc5f6323e840fa4bb536308
Hailei Wang <hwang@Retina.local>
2012-08-03 15:28:48 +0800

    Used html/template.

--- README.md
+++ README.md
@@ -2,3 +2,21 @@ Goapp
 =====
 
 A practice of GAE Go REST
+
+**TARGET:**
+To have a GAE Go REST API Service ready for Backbone SPIJA Clients development.
+
+**PROGRESS:**
+Step 1: Setup the development environment. DONE
+Step 2: Try more routes. DONE
+Step 3: Using Users
+
+Upload it to GAE and setup a test environment.
+
+**INBOX:**
+Add REST.go
+Test REST handlers
+Rewrite REST.go server
+Add Go Datastore
+Compose Go Vitaff models
+
--- hello/hello.go
+++ hello/hello.go
@@ -1,17 +1,37 @@
 package hello
 
 import "fmt"
+import "html/template"
 import "net/http"
 
+const guestbookForm = `
+	<html><body><form action="/sign" method="post">
+		<div><textarea name="content" rows="3" cols="100"></textarea></div>
+		<div><input type="submit" value="Sign Guestbook"></div>
+	</form></body></html>`
+
+const signTemplateHTML = `
+	<html><body>
+		<p>You wrote:</p>
+		<pre>{{.}}</pre>
+	</body></html>
+	`
+var signTemplate = template.Must(template.New("sign").Parse(signTemplateHTML))
+
 func init () {
-	http.HandleFunc("/", handler)
-	http.HandleFunc("/login", handleLogin)
+	http.HandleFunc("/", guest)
+	http.HandleFunc("/sign", sign)
 }
-
-func handler (w http.ResponseWriter, r *http.Request) {
-	fmt.Fprint(w, "A new start with GAE!")
+	
+func guest (rep http.ResponseWriter, req *http.Request) {
+	fmt.Fprint(rep, guestbookForm)
 }
-
-func handleLogin (res http.ResponseWriter, req *http.Request) {
-	fmt.Fprint(res, "<h1>Goapp Login</h1>")
+	
+func sign (rep http.ResponseWriter, req *http.Request) {
+	// The sign function gets the form data by calling r.FormValue.
+	// Then passes it to signTemplate.Execute that writes the rendered template to the http.ResponseWriter.
+	err := signTemplate.Execute(rep, req.FormValue("content"))
+	if err != nil {
+		http.Error(rep, err.Error(), http.StatusInternalServerError)
+	}
 }
